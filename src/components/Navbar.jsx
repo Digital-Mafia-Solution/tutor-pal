e86e16140a8bc6bc13d750ef7d3f54a960055e1c
fawkesdbs <s.mazibuko1108@gmail.com>
2025-07-18 08:01:36 +0200

    Add source code

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom';
+import logo from '../assets/logo.jpg';
+import '../App.css';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="container">
+        <Link to="/" className="logo">Tutor PAL</Link>
+        <div className="nav-links">
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
